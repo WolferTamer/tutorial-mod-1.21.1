31c6c8a7ead2f469a348dde10ab9b2858f88feed
Caleb C <wolfertamer@gmail.com>
2025-07-16 17:15:11 -0700

    Added Basic Items

--- src/main/java/net/wolfertamer/tutorialmod/TutorialMod.java
+++ src/main/java/net/wolfertamer/tutorialmod/TutorialMod.java
@@ -2,11 +2,12 @@ package net.wolfertamer.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.wolfertamer.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -15,10 +16,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
